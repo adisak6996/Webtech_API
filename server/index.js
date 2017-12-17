f661939172d641d97791ff5c8d4e03dd97fa6e57
อดิศักดิ์ พวงมณี <31331066+adisak6996@users.noreply.github.com>
2017-12-17 22:34:10 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-input
+const express = require('express')
+const bodyParser = require('body-parser')
+const contactsRouter = require('../contacts')
+const app = express()
+
+app.use(bodyparser.urlencoded({ extended: false}))
+app.use(bodyparser.json())
+
+app.use('/', contactsRouter)
+
+module.exports = app
